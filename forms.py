3f9dfc1b3bf02a6d16244af6a0a70c52098d1eb4
ConDish <samioff15@gmail.com>
2018-08-24 15:10:51 -0500

    terminate

--- forms.py
+++ forms.py
@@ -1,7 +1,13 @@
 from wtforms import Form, TextField, StringField, PasswordField
 
 
-class comment(Form):
+class registro(Form):
+    name = StringField('Name')
+    lastname = StringField('Last Name')
+    username = StringField('Username')
+    password = PasswordField('Password')
+
+class login(Form):
     username = StringField('Username')
     password = PasswordField('Password')
 
--- run.py
+++ run.py
@@ -2,59 +2,121 @@ from flask import Flask, request, render_template, redirect, url_for, session
 import json
 import os
 import forms
+import sqlite3 as sql
 
 
 app = Flask(__name__)
 app.secret_key = 'hacking'
+
+
 # host = os.environ.get('IP', 'https://python-flask-condish.c9users.io')
 # port = int(os.environ.get('PORT', 8080))
 
 
 
 
+
+
 @app.route("/", methods=['GET', 'POST'])
 def hello():
-    formulario = forms.comment(request.form)
-    otro = forms.search(request.form)
     
-    if request.method == 'POST':
-        user = formulario.username.data
-        
-        dato = json.dumps({"user":user})
-        
-        
+    search = forms.search(request.form)
     
-    return render_template("index.html", form = formulario, prueba = otro)
+    
+    return render_template("index.html", searched = search)
 
-@app.route("/prueba", methods=['GET', 'POST'])
-def prueba():
-    
+@app.route("/registro", methods=['GET', 'POST'])
+def registrar():
+    register = forms.registro(request.form)
+    search = forms.search(request.form)
+
     if request.method == 'POST':
-        user = request.form['user']
-        password = request.form['password']
-        data = {"usuario" : user, "password" : password}
+        try:
+            name = request.form['name']
+            lastname = request.form['lastname']
+            user = request.form['username']
+            password = request.form['password']
+            with sql.connect('database.db') as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO user VALUES(Null,?,?,?,?)", (name,lastname,user,password))
+                con.commit()
+                session['username'] = user
+                msg = 1
+        except:
+            con.rollback()
+            msg = 0
+        finally:
+            if msg == 1:
+
+                return redirect(url_for('ir'))
+                con.close()
+            else:
+                return redirect(url_for('registrar'))
     
-    return render_template("prueba.html", title=user)
+    
+    return render_template("registro.html", searched = search, registre = register)
+
+    
     
 
-@app.route("/hola", methods=['GET', 'POST'])
+@app.route("/logeado", methods=['GET', 'POST'])
 def ir():
-    
-    # user = request.form['username']
-    # password = request.form['password']
-    busqueda = request.form['searched']
-    session['busqueda'] = request.form['searched']
-    
-    # eliminar session = session.clear()
-    
-    # print (user, password)
-    
-    return render_template("prueba.html", search = session['busqueda'])
-    
-    
+    search = forms.search(request.form)
 
+    con = sql.connect("database.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+
+    cur.execute("select * from user")
+    rows = cur.fetchall()
+
+    if 'username' in session:
+        return render_template("logeado.html", searched = search, rows = rows, username = session["username"])
+    else:
+        return redirect(url_for('registrar'))
+
+    
+@app.route("/logout", methods=['GET', 'POST'])    
+def logout():
+    if 'username' in session:
+        session.pop('username')
+    return redirect(url_for('hello'))
+    
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+
+    login = forms.login(request.form)
+    search = forms.search(request.form)
+
+    if request.method == 'POST':
+
+        user = login.username.data
+        password = login.password.data
+
+        con = sql.connect("database.db")
+        con.row_factory = sql.Row
+
+        cur = con.cursor()
+
+        cur.execute("select * from user where username = ? and password = ?", (user,password))
+        rows = cur.fetchone()
+
+        if rows == None:
+            return redirect(url_for("login"))
+        else:
+            session['username'] = user
+            return redirect(url_for("ir"))
+        
+
+
+    return render_template("login.html", form = login , searched = search)
 
 
 
 if __name__ == "__main__":
-    app.run(debug=False, port=5000)
+
+   
+
+    
+    app.run(debug=True, port=5000)
